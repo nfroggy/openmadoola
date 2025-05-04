e3800e7726a47755d1c3f73ad589d3651a3630e0
Nathan Misner <nathan@infochunk.com>
2025-05-04 14:03:54 -0500

    make pause menu look nicer

--- src/pausemenu.c
+++ src/pausemenu.c
@@ -18,6 +18,7 @@
  */
 
 #include "constants.h"
+#include "game.h"
 #include "joy.h"
 #include "pausemenu.h"
 #include "sound.h"
@@ -25,16 +26,23 @@
 #include "task.h"
 
 #define TEXT_BASE (0x800)
-static const Uint8 palette[] = {
-    0x0F, 0x30, 0x0F, 0x0F,
+static const Uint8 menuPalette[] = {
+    0x0F, 0x16, 0x11, 0x31,
+};
+
+static const Uint8 arcadeMenuPalette[] = {
+    0x0F, 0x06, 0x11, 0x31,
+};
+
+static const Uint8 selectorPalette[] = {
     0x0F, 0x36, 0x26, 0x16,
 };
 
 static int cursor;
 
 void PauseMenu_Init(void) {
-    Sprite_SetPalette(5, palette);
-    Sprite_SetPalette(6, palette + 4);
+    Sprite_SetPalette(5, (gameType == GAME_TYPE_ARCADE) ? arcadeMenuPalette : menuPalette);
+    Sprite_SetPalette(6, selectorPalette);
     cursor = 0;
 }
 
@@ -56,9 +64,59 @@ static void PauseMenu_DrawBG(Sint16 x, Sint16 y, Sint16 w, Sint16 h) {
     Sprite spr = { 0 };
     spr.palette = 5;
     spr.size = SPRITE_8X8;
-    spr.tile = 0x7ff;
 
-    for (Sint16 i = 0; i < h; i++) {
+    // only draw side borders if we're not in arcade mode to match the hud
+    if (gameType != GAME_TYPE_ARCADE) {
+        // top left corner
+        spr.x = x - 8;
+        spr.y = y;
+        spr.tile = 0x880;
+        Sprite_DrawOverlay(&spr);
+
+        // top right corner
+        spr.x = x + w * 8;
+        spr.mirror = H_MIRROR;
+        Sprite_DrawOverlay(&spr);
+
+        // bottom right corner
+        spr.y = y + (h - 1) * 8;
+        spr.mirror = H_MIRROR | V_MIRROR;
+        Sprite_DrawOverlay(&spr);
+
+        // bottom left corner
+        spr.x = x - 8;
+        spr.mirror = V_MIRROR;
+        Sprite_DrawOverlay(&spr);
+
+        // left/right borders
+        spr.tile = 0x881;
+        for (Sint16 i = 1; i < (h - 1); i++) {
+            spr.x = x - 8;
+            spr.y = y + (i * 8);
+            spr.mirror = 0;
+            Sprite_DrawOverlay(&spr);
+            spr.x = x + w * 8;
+            spr.mirror = H_MIRROR;
+            Sprite_DrawOverlay(&spr);
+        }
+    }
+
+    // top/bottom borders
+    spr.tile = 0x882;
+    for (Sint16 i = 0; i < w; i++) {
+        spr.x = x + (i * 8);
+        spr.y = y;
+        spr.mirror = 0;
+        Sprite_DrawOverlay(&spr);
+        spr.y = y + (h - 1) * 8;
+        spr.mirror = V_MIRROR;
+        Sprite_DrawOverlay(&spr);
+    }
+
+    // fill internals
+    spr.tile = 0x7ff;
+    spr.mirror = 0;
+    for (Sint16 i = 1; i < (h - 1); i++) {
         for (Sint16 j = 0; j < w; j++) {
             spr.x = x + (j * 8);
             spr.y = y + (i * 8);
@@ -67,7 +125,7 @@ static void PauseMenu_DrawBG(Sint16 x, Sint16 y, Sint16 w, Sint16 h) {
     }
 }
 
-#define PAUSE_MENU_WIDTH 9
+#define PAUSE_MENU_WIDTH 8
 #define PAUSE_MENU_HEIGHT 5
 #define PAUSE_MENU_X_POS ((SCREEN_WIDTH / 2) - (PAUSE_MENU_WIDTH * TILE_WIDTH / 2))
 #define PAUSE_MENU_Y_POS (80)
@@ -79,11 +137,11 @@ int PauseMenu_Run(void) {
     }
 
     PauseMenu_DrawBG(PAUSE_MENU_X_POS, PAUSE_MENU_Y_POS, PAUSE_MENU_WIDTH, PAUSE_MENU_HEIGHT);
-    PauseMenu_PrintStr(PAUSE_MENU_X_POS + 16, PAUSE_MENU_Y_POS + 8, "Resume");
-    PauseMenu_PrintStr(PAUSE_MENU_X_POS + 16, PAUSE_MENU_Y_POS + 24, "Quit");
+    PauseMenu_PrintStr(PAUSE_MENU_X_POS + 13, PAUSE_MENU_Y_POS + 8, "Resume");
+    PauseMenu_PrintStr(PAUSE_MENU_X_POS + 13, PAUSE_MENU_Y_POS + 24, "Quit");
 
     Sprite spr = { 0 };
-    spr.x = PAUSE_MENU_X_POS + 8;
+    spr.x = PAUSE_MENU_X_POS + 6;
     spr.y = PAUSE_MENU_Y_POS + (cursor ? 20 : 4);
     spr.palette = 6;
     spr.size = SPRITE_8X16;
