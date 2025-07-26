e537729446a76f650ed6862e024d0b9d002cfc67
Nathan Misner <nathan@infochunk.com>
2025-07-26 13:50:31 -0500

    bump version after release

--- src/constants.h
+++ src/constants.h
@@ -30,7 +30,7 @@ typedef uint32_t Uint32;
 typedef int64_t  Sint64;
 typedef uint64_t Uint64;
 
-#define OPENMADOOLA_VERSION "1.20"
+#define OPENMADOOLA_VERSION "1.21"
 
 #define SCREEN_WIDTH (256) // original NES aspect ratio
 //#define SCREEN_WIDTH (368) // approx. 16:9 aspect ratio
